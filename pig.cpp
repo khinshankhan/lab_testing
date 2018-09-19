53b2865be1dd77d8cf34d5bbab326ad1ad2d3a40
Khinshan Khan <khanshan501@gmail.com>
2018-09-18 23:30:35 -0400

    finished piglatinify algo

--- pig.cpp
+++ pig.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 #include "pig.h"
 string piglatinify(string s){
-  return s;
+  bool constant = true; // indicates if starting letter is a constant
+  char vowels [] = {'a', 'e', 'i', 'o', 'u'}; // array of vowels to check against
+  for (int i = 0; i < 5; i++){
+    if(s.at(0) == vowels[i]){
+      constant = false; // didn't start with a vowel
+      break;
+    }
+  }
+  return (constant) ? s.substr(1) + s.at(0) + "ay" : s + "ay"; // look at README
 }
